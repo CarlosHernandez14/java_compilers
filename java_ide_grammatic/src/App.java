07fe2d153138d30b30a1bdcd396db4f14d382069
CarlosHernandez14 <carloskvpchc@gmail.com>
2025-03-19 10:45:18 -0600

    Modify grammatic to specify a file path

--- java_ide_grammatic/src/App.java
+++ java_ide_grammatic/src/App.java
@@ -1,5 +1,4 @@
 
-
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
@@ -9,14 +8,22 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        int x;
+        // int x;
         // Obtenemos el primer argumento que se le pase al programa
-        String filePath = args[0];
+        String filePath = "";
+
+        try {
+            filePath = args[0];
+        } catch (Exception e) {
+            // En caso de que no encuentre la ruta en los args cargar por defecto una
+            filePath = "C:/Users/carlo/Documents/PruebaGramaticaExamen.java";
+        }
+
         // Convertimos el archivo que se paso como argumento a un CharStream
         CharStream input = CharStreams.fromFileName(filePath);
 
         // Input stream from standard input (keyboard)
-        //CharStream input = CharStreams.fromStream(System.in);
+        // CharStream input = CharStreams.fromStream(System.in);
         sintaxisClassLexer lexer = new sintaxisClassLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         sintaxisClassParser parser = new sintaxisClassParser(tokens);
@@ -29,7 +36,7 @@ public class App {
         parser.setCustomErrorListener(errorListener);
 
         parser.program();
-        
+
         String jsonErrors = errorListener.getErrorsAsJson();
         System.out.println(jsonErrors);
 
